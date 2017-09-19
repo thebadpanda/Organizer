6f0abd69824ab8f3f112132018a95324aaa607f9
Arsenko <arsen4yk@gmail.com>
2017-09-19 22:34:29 +0300

    1

--- app/src/main/java/com/example/arsenko/organizertest/MainActivity.java
+++ app/src/main/java/com/example/arsenko/organizertest/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.arsenko.organizertest;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -16,15 +21,39 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends Activity {
 
+    //EditText et;
+
+    //ListView lvMain;
+
+    //SharedPreferences prefs;
+
+    ArrayList<String> notes;
+
+    int index = 0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences prefs = getSharedPreferences("sPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("counter", notes.size());
+        if(notes.size() > 0){
+            for(int i =0; i<notes.size(); i++){
+                editor.putString("value_", notes.get(i));
+            }
+            editor.commit();
+        }
+
+
         ListView lvMain = (ListView)findViewById(R.id.lvMain);
+
         final EditText et = (EditText)findViewById(R.id.et);
 
         final ArrayList<String> notes = new ArrayList<>();
@@ -34,45 +63,65 @@ public class MainActivity extends Activity {
 
         lvMain.setAdapter(adapter);
 
+
         et.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View view, int keyCode, KeyEvent event) {
                 if(event.getAction()==KeyEvent.ACTION_DOWN)
                     if(keyCode == KeyEvent.KEYCODE_ENTER){
-                        notes.add(0, et.getText().toString());
-                        adapter.notifyDataSetChanged();
-                        et.setText("");
-                        return true;
+                        if(et.getText().toString().matches(".*[a-zA-Zа-яА-Я0-9].*")){
+                            notes.add(et.getText().toString());
+                            adapter.notifyDataSetChanged();
+                            //adapter.add(et.getText().toString());
+                            et.setText("");
+                            index++;
+                            return true;
+                        }
+
                     }
                 return false;
+
             }
         });
+
         lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View itemClicked, int position, long id) {
                 Toast.makeText(getApplicationContext(), "кароч, давай бистра:  " + ((TextView) itemClicked).getText(), Toast.LENGTH_SHORT).show();
-//                TextView textView = (TextView)itemClicked;
-//                String insideText = textView.getText().toString(); //отримуємо текст натиснутого елемента
-//
-//                if(insideText.equalsIgnoreCase(getResources().getString(R.string.))){
-//                    startActivity(new Intent(this.MainActivity.java)); // викликаємо  актівіті
-//                }
-
-
             }
         });
 
         lvMain.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterLongView, View itemLongClicked, int position, long id) {
-                notes.remove(position);
-                adapter.notifyDataSetChanged();
+            public boolean onItemLongClick(AdapterView<?> adapterLongView, View itemLongClicked, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(itemLongClicked.getContext());
+                builder.setMessage("Delete this note?")
+                        .setPositiveButton("Yes", new AlertDialog.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                               // adapter.remove(notes.get(id));
+                                notes.remove(position);
+                                adapter.notifyDataSetChanged();
+                            }
+                }).setNegativeButton("No", null);
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
 
                 Toast.makeText(getApplicationContext(), "Видалено: " + ((TextView)itemLongClicked).getText(), Toast.LENGTH_SHORT).show();
 
                 return false;
             }
+
+
         });
 
+        }
+
+    @Override
+    public void onPause(){
+        super.onPause();
 
     }
+
 }
